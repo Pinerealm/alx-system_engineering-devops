3e37edd9b4e2adebef2081b83bb2ecf30d8c8a8e
Pinerealm <osalami09@gmail.com>
2023-01-17 00:26:11 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Get data from the REST API 'jsonplaceholder.typicode.com/'
+about a given employee ID using the requests library.
+"""
+import requests
+import sys
+
+
+def get_data():
+    """Get data from the REST API 'jsonplaceholder.typicode.com/'
+    about a given employee ID using the requests library.
+    """
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    user = requests.get(url + 'users/' + user_id).json()
+    todos = requests.get(url + 'todos?userId=' + user_id).json()
+
+    tasks = []
+    for task in todos:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get('name'), len(tasks), len(todos)))
+    for task in tasks:
+        print('\t {}'.format(task))
+
+
+if __name__ == '__main__':
+    get_data()
