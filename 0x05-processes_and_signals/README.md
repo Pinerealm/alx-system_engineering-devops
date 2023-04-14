a39cdb5a479ac575e254155bfd8ee6a477ac9113
Pinerealm <osalami09@gmail.com>
2023-04-14 13:28:48 +0300

    Remove directories

--- 0x05-processes_and_signals/README.md
+++
@@ -1,1 +0,0 @@
-## Linux processes and signals
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Create a manifest that kills a process named 'killmenow'.
-exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  path    => '/bin:/usr/bin',
-  onlyif  => 'pgrep -f killmenow',
-}
