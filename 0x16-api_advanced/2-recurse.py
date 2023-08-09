88f5e4c5af8da39d05aca64f318a380824cfca19
Pinerealm <osalami09@gmail.com>
2023-08-09 14:49:11 +0300

    Change the argument in the recursion call

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,10 +4,9 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """Returns a list of all hot posts for a given subreddit
+    """Returns recursively a list of all hot posts for a given subreddit
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit,
-                                                                 after)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {"limit": 100, "after": after}
     headers = {"User-Agent": "ALX-User"}
     response = requests.get(url, params=params, headers=headers,
@@ -16,7 +15,8 @@ def recurse(subreddit, hot_list=[], after=""):
         return None
     for post in response.json().get("data").get("children"):
         hot_list.append(post.get("data").get("title"))
+
     after = response.json().get("data").get("after")
     if after is None:
         return hot_list
-    return recurse(url, hot_list, after)
+    return recurse(subreddit, hot_list, after)
