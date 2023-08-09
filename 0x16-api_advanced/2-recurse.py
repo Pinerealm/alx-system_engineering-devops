750f53a15e070947a57e923b24c4a51cb2bd4406
Pinerealm <osalami09@gmail.com>
2023-08-09 07:40:19 +0300

    Get a list of all hot titles recursively

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""The 2-recurse module"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Returns a list of all hot posts for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit,
+                                                                 after)
+    params = {"limit": 100, "after": after}
+    headers = {"User-Agent": "ALX-User"}
+    response = requests.get(url, params=params, headers=headers,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    for post in response.json().get("data").get("children"):
+        hot_list.append(post.get("data").get("title"))
+    after = response.json().get("data").get("after")
+    if after is None:
+        return hot_list
+    return recurse(url, hot_list, after)
