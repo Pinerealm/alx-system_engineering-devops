db688496918918e04452941c4c4f3f5309cc53b8
Pinerealm <osalami09@gmail.com>
2023-08-14 07:42:10 +0300

    Change resource title

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Correct an Apache2 webserver 500 status error
-file { 'Ensure locale.php is present':
+file { 'locale.phpp':
   path   => '/var/www/html/wp-includes/class-wp-locale.phpp',
   ensure => present,
   source => '/var/www/html/wp-includes/class-wp-locale.php',
