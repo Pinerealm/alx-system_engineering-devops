9d8ee05ddb2d2a8f0e23028cdc7f294bb1861f64
Pinerealm <osalami09@gmail.com>
2023-08-14 07:56:10 +0300

    Remove the path attribute

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Correct an Apache2 webserver 500 status error
-file { 'locale.phpp':
-  path   => '/var/www/html/wp-includes/class-wp-locale.phpp',
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
   ensure => 'file',
   source => '/var/www/html/wp-includes/class-wp-locale.php',
 }
