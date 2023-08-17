ec14672d4445c02736b6e50b1670f65f9ef95e62
Pinerealm <osalami09@gmail.com>
2023-08-18 01:50:22 +0300

    Fix too many open files error for Holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Enable Holberton user login without an open file error message.
+# Increase hard file limit for Holberton user.
+exec { 'Increase hard file limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'Increase soft file limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
