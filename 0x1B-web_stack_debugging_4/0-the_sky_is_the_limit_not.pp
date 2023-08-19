ca77330c5de094407977c2805bcd3aad647b520c
Pinerealm <osalami09@gmail.com>
2023-08-19 15:02:26 +0300

    Complete sed substitution command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,13 @@
 # Configure Nginx to eliminate failed requests due to
 # 'Too many open files' errror
 exec { 'Increase open files limit':
-    command => 'sed -i s/15/1024 /etc/default/nginx',
+    command => 'sed -i s/15/1024/ /etc/default/nginx',
     path    => '/bin',
 }
 
 # Restart Nginx
 exec { 'Restart Nginx':
   command => 'sudo service nginx restart',
-  path    => '/bin',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+  require => Exec['Increase open files limit'],
 }
